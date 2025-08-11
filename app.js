2a5b68253eeee980ce7fa0802e686b6421ffa69b
Amsen <retutux@gmail.com>
2025-08-11 15:07:10 -0400

    Agrega lista dejugadores y funcion agregar jugador

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresJugadores = [];
+// funcion para asignar el elemento del DOM
+function asignarElementoDOM(elemento) { 
+    const elementoDOM = document.getElementById('amigo');
+    elementoDOM.innerHTML = elemento;
+}   
+
+// Función para agregar un jugador a la lista
+function agregarJugador() {
+    let nombre = document.getElementById('amigo').value;    
+    // Verificar si el nombre ya está en la lista  
+    if (listaNombresJugadores.includes(nombre)) {
+        console.log(`El jugador ${nombre} ya está registrado.`);
+    } else {
+        // Si el nombre no está en la lista, agregarlo
+        if (nombre.length < 3) {
+            console.log("El nombre del jugador debe tener al menos 3 caracteres.");
+            return;
+        }       
+        if (nombre.length > 20) {
+            console.log("El nombre del jugador no puede tener más de 20 caracteres.");
+            return;
+        }
+        listaNombresJugadores.push(nombre);
+        console.log(`Jugador ${nombre} agregado exitosamente.`);
+    }
+}  
+
+
+
+
+
+
